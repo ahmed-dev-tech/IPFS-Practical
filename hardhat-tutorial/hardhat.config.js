220946db3daf20cc2e63254b28f1b7b2ee7de9c8
ahmed-dev-tech <ahmedshahid0335@gmail.com>
2023-01-31 16:12:11 +0500

    done

--- hardhat-tutorial/hardhat.config.js
+++ hardhat-tutorial/hardhat.config.js
@@ -1,9 +1,9 @@
 require("@nomiclabs/hardhat-waffle");
 require("dotenv").config({ path: ".env" });
 
-const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
+const ALCHEMY_API_KEY_URL = process.env.QUICKNODE_HTTP_URL;
 
-const MUMBAI_PRIVATE_KEY = process.env.MUMBAI_PRIVATE_KEY;
+const MUMBAI_PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 module.exports = {
   solidity: "0.8.4",
